bb509e1873f4cb3e8beaaae0743509a439b04397
GolovRibolov <kirgo12345@mail.ru>
2021-12-29 00:19:20 +0300

    golov3

--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -1,6 +1,24 @@
-// реализация пользовательского приложения
+﻿#include <iostream>
+#include <string>
+#include "arithmetic.h"
+
+using namespace std;
 
 int main()
 {
+  string expression;
+  TPostfix postfix;
+  double res;
+
+  setlocale(LC_ALL, "Russian");
+  cout << "Введите арифметическое выражение: ";
+  getline(cin, expression);
+  //cout << expression << endl;
+  postfix.ToPostfix(expression);
+  cout << "Арифметическое выражение: " << postfix.GetInfix() << endl;
+  cout << "Постфиксная форма: " << postfix.GetPostfix() << endl;
+  res = postfix.Calculate();
+  cout << res << endl;
+
   return 0;
 }
